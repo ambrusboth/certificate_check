c4763fe7fda26cf52582e3f73e7920f33cc78422
Ambrus Both <ambrus.both@kiongroup.com>
2025-02-18 14:34:06 +0100

    Add example API urls

--- 03b_plain_python.py
+++ 03b_plain_python.py
@@ -9,6 +9,16 @@ if __name__ == "__main__":
         os.environ[ENV_KEY] = certifi.where()
     print(f"{ENV_KEY} = '{os.environ[ENV_KEY]}'")
 
-    URL = "https://example.com/api/endpoint"
-    response = requests.get(URL, verify=certifi.where(), timeout=30)
+    # URL = "https://example.com/api/endpoint"
+    # URL = "https://api.github.com"
+    URL = "https://official-joke-api.appspot.com/random_joke"
+    # URL = "https://dog.ceo/api/breeds/image/random"
+    # URL = "https://datausa.io/api/data?drilldowns=Nation&measures=Population"  # This works in the browser but not in the code
+    response = requests.get(
+        URL,
+        verify=certifi.where(),
+        #    cert="C:\\Users\\a0102487\\Downloads\\zscaler_try.cer",
+        timeout=30,
+    )
     print(response.status_code)
+    print(response.text)
